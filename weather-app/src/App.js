86501278bbbf2eb7675ba09997e28da6f5b1b77d
ganapa11 <ganapa11@msu.edu>
2024-12-11 20:25:09 -0500

    Added new components and reorganized the pages

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,52 +1,15 @@
 import "./App.css";
-import { useState } from "react";
-import { Box, Button, Typography } from "@mui/material";
-import DarkModeIcon from "@mui/icons-material/DarkMode";
-import LightModeIcon from "@mui/icons-material/LightMode";
-import { lightTheme, darkTheme } from "./theme/colorTheme";
-import { WeatherSelection } from "./weather-selection/weatherSelection";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./home-page/HomePage";
 
 function App() {
-  const [colorMode, setColorMode] = useState(lightTheme);
-
-
-  const toggleColorMode = () => {
-    setColorMode(colorMode === lightTheme ? darkTheme : lightTheme);
-  };
-
   return (
-    <div className="App">
-      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Parkinsans:wght@300..800&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
-        rel="stylesheet"
-      ></link>
-      <Box className="weather-location-container">
-        <WeatherSelection />
-      </Box>
-
-      <Box className="title-container">
-        <Box className="color-mode-container">
-          <Button
-            className="color-mode-button"
-            variant="outlined"
-            sx={{ borderRadius: 50, borderColor: "white" }}
-            onClick={toggleColorMode}
-          >
-            {colorMode === lightTheme ? (
-              <DarkModeIcon sx={{ color: "white" }} />
-            ) : (
-              <LightModeIcon />
-            )}
-          </Button>
-        </Box>
-
-        <Typography variant="h3" component="h2" className="title">
-          Welcome to EasyWeather!
-        </Typography>
-      </Box>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ weather-app/src/home-page/HomePage.js
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+export const Home = () => {
+  return (
+    <Box>
+      <Box>we</Box>
+    </Box>
+  );
+};
