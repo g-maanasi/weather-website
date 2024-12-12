96e79d115a4aa48eabb0074fcf92e09ab4781767
ganapa11 <ganapa11@msu.edu>
2024-12-11 21:24:28 -0500

    Added homepage structure

--- weather-app/src/home-page/HomePage.js
+++ weather-app/src/home-page/HomePage.js
@@ -1,8 +1,37 @@
-import { Box } from "@mui/material";
+import "./homepage.css";
+import { Box, Typography } from "@mui/material";
+import ThermostatIcon from "@mui/icons-material/Thermostat";
+
 export const Home = () => {
   return (
-    <Box>
-      <Box>we</Box>
+    <Box className="home-container">
+      <Box className="header-bar">
+        <Box className="header-navigation">
+          <Box className="logo">
+            <ThermostatIcon
+              className="logo-icon"
+              sx={{ height: 35, width: 35 }}
+            />
+            <Typography
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                ml: 1,
+                fontSize: 20,
+                fontFamily: "'DM Sans', sans-serif",
+              }}
+            >
+              National Weather
+            </Typography>
+          </Box>
+
+          <Box className="log-in-container">
+              
+          </Box>
+        </Box>
+      </Box>
+
+      <Box className="middle-container"></Box>
     </Box>
   );
 };
