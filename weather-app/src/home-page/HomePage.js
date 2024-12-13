6d56bc7f88847345a339690b7a6ffa78aa344c04
ganapa11 <ganapa11@msu.edu>
2024-12-13 15:54:25 -0500

    Added log in link and non functional search bar

--- weather-app/src/home-page/HomePage.js
+++ weather-app/src/home-page/HomePage.js
@@ -1,6 +1,16 @@
 import "./homepage.css";
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Link,
+  Typography,
+  TextField,
+  InputAdornment,
+  FormControl,
+  OutlinedInput,
+  FormHelperText,
+} from "@mui/material";
 import ThermostatIcon from "@mui/icons-material/Thermostat";
+import SearchIcon from "@mui/icons-material/Search";
 
 export const Home = () => {
   return (
@@ -25,8 +35,23 @@ export const Home = () => {
             </Typography>
           </Box>
 
-          <Box className="log-in-container">
-              
+          <Box className="right-container">
+            <Box className="search-bar">
+              <FormControl sx={{ m: 1, width: "24ch" }} variant="outlined">
+                <OutlinedInput
+                  id="outlined-adornment-weight"
+                  startAdornment={<InputAdornment position="start"><SearchIcon /></InputAdornment>}
+                  aria-describedby="outlined-weight-helper-text"
+                  inputProps={{
+                    "aria-label": "weight",
+                  }}
+                  size="small"
+                />
+              </FormControl>
+            </Box>
+            <Box className="log-in-container">
+              <Link href="login">Log In or Sign Up</Link>
+            </Box>
           </Box>
         </Box>
       </Box>
