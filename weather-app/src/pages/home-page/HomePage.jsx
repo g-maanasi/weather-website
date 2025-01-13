7785268bd91b71d562e81e14a3bd3b5e362201ed
Maanasi Ganapathy <maanasiganapathy@gmail.com>
2025-01-12 20:25:06 -0600

    Added get started button

--- weather-app/src/pages/home-page/HomePage.jsx
+++ weather-app/src/pages/home-page/HomePage.jsx
@@ -1,10 +1,17 @@
 import "./homepage.css";
-import { Box, Typography } from "@mui/material";
+import React, { useRef } from 'react';
+import { Box, Button, Typography } from "@mui/material";
 import { NavigationBar } from "../../components/navigation/NavigationBar";
 import { LocationSelection } from "../../components/weather-selection/LocationSelection";
 import { Fade } from "react-awesome-reveal";
 
 export const Home = () => {
+  const ref = useRef(null);
+
+  const handleClick = () => {
+    ref.current?.scrollIntoView({behavior: 'smooth'});
+  };
+
   return (
     <Box className="home-container">
       <NavigationBar />
@@ -12,17 +19,29 @@ export const Home = () => {
       <Box className="page-container">
         <Box className="banner-container">
           <Box className="banner">
-            <Fade cascade damping={0.7} className="text-containter">
+            <Fade cascade damping={0.6} className="text-containter">
               <Typography variant="h4" fontWeight={"fontWeightLight"}>
                 Making weather lookup
               </Typography>
-              <Typography variant="h3" fontWeight={"fontWeightHeavy"} sx={{ color: '#02387f' }}>
+              <Typography
+                variant="h3"
+                fontWeight={"fontWeightHeavy"}
+                sx={{ color: "#02387f" }}
+              >
                 Simple and Seamless
               </Typography>
+              <Button
+                variant="contained"
+                size="large"
+                onClick={handleClick}
+                sx={{ mt: "2rem", backgroundColor: "black" }}
+              >
+                Get Started
+              </Button>
             </Fade>
           </Box>
         </Box>
-        <LocationSelection />
+        <LocationSelection ref={ref} />
       </Box>
     </Box>
   );
