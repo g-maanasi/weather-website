0e7d73241c5acb5f69f8e9d4b639c8ad6b9270f0
Maanasi Ganapathy <maanasiganapathy@gmail.com>
2025-01-18 15:01:25 -0600

    Got rid of banner

--- weather-app/src/pages/home-page/HomePage.jsx
+++ weather-app/src/pages/home-page/HomePage.jsx
@@ -3,7 +3,6 @@ import React, { useRef } from 'react';
 import { Box, Button, Typography } from "@mui/material";
 import { NavigationBar } from "../../components/navigation/NavigationBar";
 import { LocationSelection } from "../../components/weather-selection/LocationSelection";
-import { Fade } from "react-awesome-reveal";
 
 export const Home = () => {
   const ref = useRef(null);
@@ -17,30 +16,6 @@ export const Home = () => {
       <NavigationBar />
 
       <Box className="page-container">
-        <Box className="banner-container">
-          <Box className="banner">
-            <Fade cascade damping={0.6} className="text-containter">
-              <Typography variant="h4" fontWeight={"fontWeightLight"}>
-                Making weather lookup
-              </Typography>
-              <Typography
-                variant="h3"
-                fontWeight={"fontWeightHeavy"}
-                sx={{ color: "#02387f" }}
-              >
-                Simple and Seamless
-              </Typography>
-              <Button
-                variant="contained"
-                size="large"
-                onClick={handleClick}
-                sx={{ mt: "2rem", backgroundColor: "black" }}
-              >
-                Get Started
-              </Button>
-            </Fade>
-          </Box>
-        </Box>
         <LocationSelection ref={ref} />
       </Box>
     </Box>
